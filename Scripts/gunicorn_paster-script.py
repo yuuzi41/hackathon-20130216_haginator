163fbdf9b5261d0b2afd7eabb56ef6561446dd70
yuuzi <yuuzi41@hotmail.com>
2013-02-18 19:56:31 +0900

    use virtualenv, add gunicorn, use relational path

---
+++ Scripts/gunicorn_paster-script.py
@@ -0,0 +1,9 @@
+#!C:\Users\yhagiwara\Documents\hackathon_20130216\hackathon-20130216_haginator\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'gunicorn==0.17.2','console_scripts','gunicorn_paster'
+__requires__ = 'gunicorn==0.17.2'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('gunicorn==0.17.2', 'console_scripts', 'gunicorn_paster')()
+)
