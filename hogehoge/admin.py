e455ca7a7e8a836c0eabece2ec77ed8fe02e5ea5
mu248321 <s1120755@u.tsukuba.ac.jp>
2013-02-22 19:37:32 +0900

    add ProbDist to AdminSite

--- hogehoge/admin.py
+++ hogehoge/admin.py
@@ -1,9 +1,9 @@
-from hogehoge.models import Questions, Musics, Samples
+from hogehoge.models import Questions, Musics, Samples, ProbDist
 from django.contrib import admin
 
 admin.site.register(Musics)
 admin.site.register(Questions)
 admin.site.register(Samples)
-
+admin.site.register(ProbDist)
 
 
